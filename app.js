c0735e376419eb2ded438924147b464b24ead9cb
Robson Durães <duraesrobson@gmail.com>
2025-02-06 20:09:20 -0300

    criação das funções para atualizar a lista de amigos e sortear o amigo secreto;

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 let amigos = [];
 
-
 function adicionarAmigo() {
     let amigo = document.getElementById('amigo').value;
 
-    if (amigo == "") {
-        alert("Digite um nome")
-    } else{
+    if (amigo == ''){
+        alert("Digite um nome");
+
+    } else if(amigos.includes(amigo)) {
+        alert("Esse nome já foi digitado");
+    } else {
         amigos.push(amigo);
-        document.getElementById('amigo').value = ""
+        document.getElementById('amigo').value = '';
+        atualizarListaAmigos();
     }
 
     console.log(amigos);
+}
+
+
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
+
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert("Sem nomes para sortear")
+        return;
+    }
+
+    let amigoAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[amigoAleatorio];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+    document.getElementById('amigo').value = '';
 
 }
